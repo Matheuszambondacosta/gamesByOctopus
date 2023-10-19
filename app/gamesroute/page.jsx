d53d5de95f9d45469a193fdd89c4427d7ca5ec12
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2023-10-19 20:10:48 -0300

    feat: filtering by copilot

--- app/gamesroute/page.jsx
+++ app/gamesroute/page.jsx
@@ -1,32 +1,42 @@
 "use client";
 import React, { useState, useEffect } from 'react';
 import styles from './page.module.css';
-import { fetchApi } from '@/data/apiconsumer';
+import fetchApi from '@/data/apiconsumer';
 
 function gamePage() {
-  const [games, setGames] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [selectedPlatform, setSelectedPlatform] = useState("all");
-  const [selectedGenre, setSelectedGenre] = useState("all");
-  const [selectedRating, setSelectedRating] = useState("all");
+    const [games, setGames] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [selectedPlatform, setSelectedPlatform] = useState("all");
+    const [selectedGenre, setSelectedGenre] = useState("all");
+    const [selectedRating, setSelectedRating] = useState("all");
 
-  useEffect(() => {
-    const gamesFetch = async () => {
-      try {
-        const dados = await fetchApi();
-        setGames(dados);
-      } catch (error) {
-        throw error;
-      }
-    };
+    useEffect(() => {
+        const gamesFetch = async () => {
+            try {
+                const dados = await fetchApi();
+                setGames(dados);
+            } catch (error) {
+                throw error;
+            }
+        };
 
-    gamesFetch();
-  }, []);
-    return (
-        <div>
-            <h1>Game Page</h1>
-        </div>
-    );
-}
+        gamesFetch();
+    }, []);
+
+    const filterGames = games.filter((game) => {
+    const platformName = game.platforms.map((platform) => platform.platform.name);
+    const gameGenres = game.genres.map((genre) => genre.name);
+    const platformFilter = selectedPlatform == "all" || platformName.includes(selectedPlatform);
+    const genreFilter = selectedGenre == "all" || gameGenres.includes(selectedGenre);
+    const ratingFilter = selectedRating == "all" || game.rating == selectedRating;
+    return platformFilter && genreFilter && ratingFilter;
+    });
+
+        return (
+            <main className={styles.main}>
+                <h1>Game Page</h1>
+            </main>
+        );
+    }
 
 export default gamePage;
