f48d3f373ca04efe6b27ab420431df46d82ca4a4
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2023-10-19 18:56:20 -0300

    feat: consuming api in gamesroute

--- app/gamesroute/page.jsx
+++ app/gamesroute/page.jsx
@@ -1,8 +1,26 @@
 "use client";
 import React, { useState, useEffect } from 'react';
+import styles from './page.module.css';
 
 function gamePage() {
-    const [games, setGames] = useState([]);
+  const [games, setGames] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedPlatform, setSelectedPlatform] = useState("all");
+  const [selectedGenre, setSelectedGenre] = useState("all");
+  const [selectedRating, setSelectedRating] = useState("all");
+
+  useEffect(() => {
+    const gamesFetch = async () => {
+      try {
+        const dados = await fetchApi();
+        setGames(dados);
+      } catch (error) {
+        throw error;
+      }
+    };
+
+    gamesFetch();
+  }, []);
     return (
         <div>
             <h1>Game Page</h1>
