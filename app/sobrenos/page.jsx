fcda8f2246cea7c6a315b0f00b03b541bc52b8eb
João Victor <joaosantos.senai564@gmail.com>
2023-10-20 13:43:30 -0300

    sobre nos 99%

--- app/sobrenos/page.jsx
+++ app/sobrenos/page.jsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 function SobreNos() {
     return (
         <main className={Styles.main1}>
+   {/* <Header/> */}
             <h1 className={Styles.titulo}>Sobre Nós</h1>
 
             <div className={Styles.container}>
@@ -27,7 +28,7 @@ function SobreNos() {
                     </div>
                     <div className={Styles.card}>
                         <a href="https://www.instagram.com/mathh_costa" target='_blank'><Image className={Styles.img} src="/pictureprofile.octopus03.jfif" width={180} height={180} /></a>
-                        <p className={Styles.text1}><strong>Nome: </strong> Matheus Zambon</p>
+                        <p className={Styles.text1}><strong>Nome: </strong> Matheus Zambom</p>
                         <p className={Styles.text1}><strong>Idade:</strong> 17 anos</p>
                         <p className={Styles.text1}><strong>Descrição:</strong>Conheça Matheus, um estudante dedicado que está trilhando seu caminho no mundo da tecnologia. Ele é aluno do colégio Sesi, onde absorve conhecimento além das disciplinas convencionais. Nos intervalos, você o encontrará imerso no mundo do Senai, aprimorando suas habilidades no curso de Desenvolvimento de Sistemas.</p>
                     </div>
