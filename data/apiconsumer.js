6de60e7d5076b4eac6fb1fdf2d7317e1664b9e75
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2023-10-19 22:15:35 -0300

    feat: dj khaled

--- data/apiconsumer.js
+++ data/apiconsumer.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const URL_API = 'https://api.rawg.io/api/games?key=2014ca8f1ab34906952da5f330744b55'
+const URL_API = 'https://api.rawg.io/api/games?key=2014ca8f1ab34906952da5f330744b55&dates=2023-01-01,2023-10-19&ordering=-added';
 
 
 const fetchApi = async () => {
