5ed222ff97964bcd323b0b2514cb860b11896fbf
arthurborges <arthurborges.senai564@gmail.com>
2023-10-23 15:36:12 -0300

    feat: new api key

--- data/apiconsumer.js
+++ data/apiconsumer.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const fetchApi = async (page) => {
-    const URL_API = `https://api.rawg.io/api/games?key=2014ca8f1ab34906952da5f330744b55&page=${page}`;
+    const URL_API = `https://api.rawg.io/api/games?key=219af9c69fd8413cb6d8e15b6471d74a&page=${page}`;
 
     try {
         const resposta = await axios.get(URL_API);
@@ -15,7 +15,7 @@ export const fetchApi = async (page) => {
 
 export const fetchApiDetails = async (id) => {
     try {
-        const resposta = await axios.get(`https://api.rawg.io/api/games/${id}?key=2014ca8f1ab34906952da5f330744b55`);
+        const resposta = await axios.get(`https://api.rawg.io/api/games/${id}?key=219af9c69fd8413cb6d8e15b6471d74a`);
         return resposta.data;
     }
 
--- data/gamedata.js
+++ data/gamedata.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const fetchAsyncGames = async (page = 1) => {
-    const URL_API = `https://api.rawg.io/api/games?key=2014ca8f1ab34906952da5f330744b55&page=${page}`;
+    const URL_API = `https://api.rawg.io/api/games?key=219af9c69fd8413cb6d8e15b6471d74a&page=${page}`;
     const { data } = await axios.get(URL_API);
     return data;
   };
